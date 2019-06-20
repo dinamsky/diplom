13a5c6977ecdcf572e8789693227e4c0b2b8642d
MikhailYunin <kokosiam@gmail.com>
2019-06-20 09:30:59 +0300

    commit9// переделка на DAO/все еще не работает

--- src/main/java/com/restik/mydiplom/controller/HomeController.java
+++ src/main/java/com/restik/mydiplom/controller/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home(Locale locale, Model model) {
 
-        return "index";
+        return "add_user";
     }
 
 }
